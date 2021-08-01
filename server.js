d00a3efe94b9777a640078ca535d0837703c6255
rongni <rni4@illinois.edu>
2021-08-01 16:54:35 -0500

    updated_yarn

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use('/api/notes', require('./routes/api/notes'));
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 
